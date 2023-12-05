8b5e6bc77766512c6515a5102834dabbaac6eed8
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-05 18:02:45 +0100

    Do day 2

--- day2.py
+++ day2.py
@@ -135,6 +135,18 @@ class Game:
     def from_integer_and_bags(cls, integer: int, contents: list[BagContent]) -> Game:
         return Game(integer, contents)
 
+    def is_possible_with(self, bag: BagContent) -> bool:
+        if len(self.bags) == 0:
+            return False
+        return all(revealed_bag <= bag for revealed_bag in self.bags)
+
+    def get_lower_bound(self) -> BagContent:
+        return BagContent(
+            red=max(bag.red for bag in self.bags),
+            blue=max(bag.blue for bag in self.bags),
+            green=max(bag.green for bag in self.bags),
+        )
+        return self.bags[0]
 
 
 # (Game.from_integer . int) <$> ('Game ' *> digit <* ':')
@@ -153,6 +165,9 @@ class BagContent:
     blue: int = 0
     green: int = 0
 
+    def power(self) -> int:
+        return self.red * self.blue * self.green
+
     @classmethod
     def from_int_and_str(cls, integer, string) -> BagContent:
         if string == 'red':
@@ -163,6 +178,9 @@ class BagContent:
             return BagContent(green=integer)
         raise Exception
 
+    def __le__(self, other):
+        return self.red <= other.red and self.blue <= other.blue and self.green <= other.green
+
 
 
 def sum_bags(bags: list[BagContent]) -> BagContent:
@@ -171,19 +189,17 @@ def sum_bags(bags: list[BagContent]) -> BagContent:
     green = sum(bag.green for bag in bags)
     return BagContent(red=red, blue=blue, green=green)
 
-game_id = apply(
-    int, 
-    left(
-        right(word('Game '), digit), 
-        word(': '),
-    )
-)
-
-
 integer = apply(
     int, 
     apply(''.join, many(digit))
 )
+
+game_id = left(
+    right(word('Game '), integer), 
+    word(': '),
+)
+
+
 blue = word('blue')
 green = word('green')
 red = word('red')
@@ -201,7 +217,6 @@ game = and_(game_id, bag_contentss, combiner=Game.from_integer_and_bags)
 
 
 
-
 """
 Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
@@ -210,9 +225,33 @@ Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 """
 
-print(game_id('Game 1: '))
-print(bag_content('3 green'))
-print(bag_contentss('3 green, 5 blue; 2 red'))
-print(game('Game 1: 3 green, 5 blue; 20 red'))
 
+def collect_possible_games(unparsed_games: list[str], bag: BagContent) -> int:
+    games = [game(unparsed_game).result for unparsed_game in unparsed_games]
+    return sum(game.id_ for game in games if game.is_possible_with(bag))
+
+assert not Game(id_=1, bags=[]).is_possible_with(BagContent(1, 2, 3))
+assert not Game(id_=1, bags=[BagContent(12, 2, 3)]).is_possible_with(BagContent(1, 2, 3))
+assert collect_possible_games(
+    [
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 9 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    ],
+    BagContent(blue=8, red=100, green=100)
+) == 1
+assert Game(id_=1, bags=[BagContent(12, 2, 3)]).get_lower_bound() == BagContent(12, 2, 3)
+assert Game(
+    id_=1, 
+    bags=[
+        BagContent(12, 2, 3),
+        BagContent(11, 4, 3),
+    ]
+).get_lower_bound() == BagContent(12, 4, 3)
+assert BagContent(12, 2, 3).power() == 12 * 2 * 3
+
+
+with open('day2_input', 'r') as f:
+    unparsed_games = [line for line in f]
+
+print(collect_possible_games(unparsed_games, BagContent(red=12, blue=14, green=13)))
 
