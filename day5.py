ae83514581f69d1ee1c0e4804752aa90f058cd46
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-17 11:18:48 +0100

    buh

--- day5.py
+++ day5.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from interval import Interval
+from interval import Interval, explode
 from parsing import *
 
 example_data = """seeds: 79 14 55 13
@@ -48,14 +48,32 @@ seeds = apply(set, right(word("seeds: "), separated_by(interval, ' ')))
 
 number_map_line: Parser[dict[Interval, int]] = apply(lambda ints: {Interval.from_start_and_size(ints[1], ints[2]): ints[0]}, separated_by(integer, ' '))
 number_map_lines = apply(
-    lambda x: {k: v for line in x for k, v in line.items()},
+    lambda x: {interval_: offset for line in x for interval_, offset in line.items()},
     separated_by(number_map_line, '\n')
 )
 
+NumberMap = dict[Interval, int]
 thingy = or_(*map(word, ['soil', 'seed', 'humidity', 'fertilizer', 'location', 'water', 'light', 'temperature']))
 number_map_header = and_(and_(and_(thingy, word('-to-')), thingy), word(' map:\n'))
-number_map = right(number_map_header, number_map_lines)
+number_map: Parser[NumberMap] = right(number_map_header, number_map_lines)
 number_maps = apply(tuple, separated_by(number_map, '|'))
+
+
+def apply_number_map(number_map: NumberMap, interval: Interval) -> set[Interval]:
+    if len(number_map) == 0:
+        return {interval}
+    sub_intervals = explode(interval, intervals_to_explode_by=set(number_map))
+    return {
+        apply_number_map_to_sub_interval(number_map, sub_interval)
+        for sub_interval in sub_intervals
+    }
+
+
+def apply_number_map_to_sub_interval(number_map: NumberMap, interval: Interval) -> Interval:
+    for map_interval, offset in number_map.items():
+        if not map_interval.disjoint(interval):
+            return interval.move(offset)
+    return interval
 
 
 assert seeds("seeds: 79 3 55 1").result == {Interval(79, 81), Interval(55, 55)}
@@ -65,5 +83,11 @@ assert number_maps("water-to-location map:\n60 56 37\n9 9 9|seed-to-soil map:\n9
     {Interval(56, 92): 60, Interval(9, 17): 9},
     {Interval(9, 17): 9},
 ), number_maps("water-to-location map:\n60 56 37\n9 9 9|seed-to-soil map:\n9 9 9").result
+
+assert apply_number_map(dict(), Interval(3, 9)) == {Interval(3, 9)}
+assert apply_number_map({Interval(3, 9): 1}, Interval(3, 9)) == {Interval(4, 10)}
+assert apply_number_map({Interval(3, 9): 1}, Interval(3, 9)) == {Interval(4, 10)}
+assert apply_number_map({Interval(100, 101): 1}, Interval(3, 9)) == {Interval(3, 9)}
+assert apply_number_map({Interval(3, 4): 1}, Interval(3, 9)) == {Interval(4, 5), Interval(5, 9)}
 
 assert lowest_location(example_data) == 46, lowest_location(example_data)
