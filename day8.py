762d96981aaf73cbca56a12a75a27bce59c4f914
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-12-19 00:52:00 +0100

    Naive solution which would work with a better laptop

--- day8.py
+++ day8.py
@@ -1,4 +1,5 @@
 from ast import literal_eval
+from typing import Iterator
 from itertools import cycle, count
 
 with open('day8_input') as f:
@@ -9,5 +10,23 @@ current = 'AAA'
 for score, instruction in zip(count(1), cycle(instructions)):
     current = map_[current][int(instruction)]
     if current == 'ZZZ':
+        print(score)
+        break
+
+starting_points = [x for x in map_ if x.endswith('A')]
+print(starting_points)
+
+
+def traverse(starting_point: str) -> Iterator[str]:
+    current = starting_point
+    for instruction in cycle(instructions):
+        current = map_[current][int(instruction)]
+        yield current
+
+
+traversals = map(traverse, starting_points)
+
+for score, *itineraries in zip(count(1), *traversals):
+    if all(node.endswith('Z') for node in itineraries):
         print(score)
         break
